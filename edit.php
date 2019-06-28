2b8c5225ecf019d2bf702d59f9d008c03295640b
Viviane Nonato <nonato.viviane@gmail.com>
2019-06-28 10:20:46 -0300

    Edita

--- edit.php
+++ edit.php
@@ -4,3 +4,4 @@ $db_handle = new DBController();
 $sql = "UPDATE posts set " . $_POST["column"] . " = '".$_POST["editval"]."' WHERE  id=".$_POST["id"];
 $result = $db_handle->executeUpdate($sql);
 ?>
+
